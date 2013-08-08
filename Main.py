3a344453d4407db40a40032b56d746f02bb680cd
Jason Hou <houkelei@gmail.com>
2013-08-08 10:02:05 +0800

    open HTMLTestrunner support

--- Main.py
+++ Main.py
@@ -6,24 +6,24 @@ try:
 except:
 	pass
 from com.android.monkeyrunner import MonkeyRunner,MonkeyDevice,MonkeyImage
-from log import trace
 import contacts
-# import HTMLTestRunner
+import HTMLTestRunner
 import unittest
 
-# rootPath = os.path.dirname(os.path.abspath(__file__))
-# reportFile = rootPath + os.sep + 'TestReport.html'
+HTML_SUPPORT = False
 
-device=MonkeyRunner.waitForConnection(5,'0a78310541615517')
+rootPath = os.path.dirname(os.path.abspath(__file__))
+reportFile = rootPath + os.sep + 'TestReport.html'
+
+device=MonkeyRunner.waitForConnection(5,'045070c5429fb317')
 if not device:
 	print >> sys.stderr,"connection failed"
 	sys.exit(1)
-c=contacts.contacts(device,'0a78310541615517')
-c.start()
+
 contactTest={
-	'Name':'Tom',
-	'Phone':'12453',
-	'Website':'cctv',
+	'Name':'Jason',
+	'Phone':'1234567890',
+	'Website':'www.baidu.com',
 	'Notes':'test'
 }
 
@@ -39,10 +39,11 @@ class test_demo(unittest.TestCase):
 		self.assertEqual(before-after,1)
 
 	def test_add(self):
-		before=c.getCounter()
-		c.addContact(**contactTest)
-		after=c.getCounter()
-		self.assertEqual(after-before,1)
+		for i in range(3):
+			before=c.getCounter()
+			c.addContact(**contactTest)
+			after=c.getCounter()
+			self.assertEqual(after-before,1)
 
 	def test_edit(self):
 		c.editDetails('Tom',Phone='10086')
@@ -54,30 +55,35 @@ class test_demo(unittest.TestCase):
 		c.sortAndViewAs(sort=False,first=False)
 
 	def test_snap(self):
-		c.snapshot('test')
+		c.snapshot('snaptest')
+        
+    @classmethod
+    def setUpClass(cls):
+        c=contacts.contacts(device,'045070c5429fb317')
+        c.start()
+    
+    @classmethod
+    def tearDownClass(cls):
+        c.stop()
 
 if __name__ == '__main__':
 
-	# suite = unittest.TestLoader().loadTestsFromTestCase(test_demo)
-	# unittest.TextTestRunner(verbosity=2).run(suite)
-
-	# suite=unittest.TestSuite()
-	# suite.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(test_demo)])
-	# unittest.TextTestRunner(verbosity=2).run(suite)
 	suite=unittest.TestSuite()
+	# suite.addTest(test_demo("test_add"))
 	# suite.addTest(test_demo("test_favor"))
 	# suite.addTest(test_demo("test_delete"))
-	suite.addTest(test_demo("test_snap"))
-	unittest.TextTestRunner(verbosity=2).run(suite)
-	'''
-	outfile = open(reportFile, "w")
-	runner = HTMLTestRunner.HTMLTestRunner(
-			stream=outfile,
-			title='Test Report',
-			description='Test'
-			)
-	print 'Now start testing ...'
-	runner.run(suite)
-	# c.stop()
-	print 'Testing completed, press any key to exit!'
-	'''
+	# suite.addTest(test_demo("test_snap"))
+	# suite.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(test_demo)])
+	# suite = unittest.TestLoader().loadTestsFromTestCase(test_demo)
+	if not HTML_SUPPORT:
+		unittest.TextTestRunner(verbosity=2).run(suite)
+	else:
+		outfile = open(reportFile, "w")
+		runner = HTMLTestRunner.HTMLTestRunner(
+				stream=outfile,
+				title='Test Report',
+				description='Test'
+				)
+		print 'Now start testing ...'
+		runner.run(suite)
+		print 'Testing completed, press any key to exit!'
